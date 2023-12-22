5ed507c98ec40edd1448c706f2899d9c75a80351
ZReticules <my_mailbox_228@mail.ru>
2023-12-22 10:31:53 +0300

    Документированы новые инструкции

--- Data/iverilog/bin/testbench_2.0.sv
+++ Data/iverilog/bin/testbench_2.0.sv
@@ -14,11 +14,12 @@ module SimpleProcessor_tb;
     reg [15:0] vidmem [3:0];
 
     //MemStart
-    parameter MemStart = 15'h00;
-    //'Interrupts'
-    parameter HexPrint = 15'h01;
-    parameter DecPrint = 15'h02;
-    parameter CharPrint = 15'h03;
+    parameter MemStart = 16'd65535 - 15'd256;
+    //'Interrupts'    
+    parameter BinPrint = 16'h02;
+    parameter CharPrint = 16'h08;
+    parameter DecPrint = 16'h0A;
+    parameter HexPrint = 16'h10;
 
     // Instantiate the SimpleProcessor module
     SimpleProcessor dut (
@@ -39,17 +40,21 @@ module SimpleProcessor_tb;
 
     always @(data_out) begin
         case (data_out)
+            BinPrint:begin
+                mem_sel = MemStart;
+                $write("%b", mem_out);
+            end
             HexPrint:begin
                 mem_sel = MemStart;
                 $write("%x", mem_out);
             end
             DecPrint:begin
                 mem_sel = MemStart;
-                $write("%d", mem_out[15:0]);
+                $write("%d", mem_out);
             end
             CharPrint:begin
                 mem_sel = MemStart;
-                $write("%c", mem_out[15:0]);
+                $write("%c", mem_out[7:0]);
             end
         endcase
     end
